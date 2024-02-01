5e3968f1a71abe0376a9aeae604298c9dae59caa
nityak1996 <nityak1996@gmail.com>
2024-02-01 15:29:59 +0530

    sigin/signup form && Login/header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import "./App.css";
+
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="text-3xl text-green-800 font-bold">Namaste Netflix Gpt</div>
+    <Body/>
   );
 }
 
